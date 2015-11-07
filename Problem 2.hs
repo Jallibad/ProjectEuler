dd91fdeb442f703e246d53ce6e301a7939dc976d
Jordan <jallibad@gmail.com>
2015-11-07 17:17:09 -0500

    Switched more problems to modules

--- Problem 2.hs
+++ Problem 2.hs
@@ -1,2 +1,3 @@
+import MathFunctions
+
 main = print $ sum $ filter even $ takeWhile (<4*10^6) $ fibs
-	where fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
--- Problem 7.hs
+++ Problem 7.hs
@@ -1,10 +1,3 @@
-isqrt :: Integral (a) => a -> a
-isqrt 0 = 0
-isqrt 1 = 1
-isqrt n = head $ dropWhile (\x -> x*x > n) $ iterate (\x -> (x + n `div` x) `div` 2) (n `div` 2)
+import MathFunctions
 
-isPrime :: Integral (a) => a -> Bool
-isPrime 1 = False
-isPrime number = all ((/=) 0 . mod number) [2..isqrt number]
-
-main = print $ filter isPrime [1..] !! 10000
+main = print $ primes !! 10000
