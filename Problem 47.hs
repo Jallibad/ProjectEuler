ecb53572f9740742fdb22d1a0a44e88479dab861
Jordan <jallibad@gmail.com>
2015-11-11 19:44:39 -0500

    Redid Problem 47

--- Problem 47.hs
+++ Problem 47.hs
@@ -1,20 +1,9 @@
-isqrt 0 = 0
-isqrt 1 = 1
-isqrt n = head $ dropWhile (\x -> x*x > n) $ iterate (\x -> (x + n `div` x) `div` 2) (n `div` 2)
+import Data.List
+import MathFunctions
 
-factors :: Integral (a) => a -> [a]
-factors number = lowerFactors ++ if ((head upperFactors)^2 == number) then (tail upperFactors) else upperFactors
-	where	lowerFactors = filter ((==) 0 . rem number) [1..isqrt number]
-		upperFactors = reverse $ map (div number) lowerFactors
+primeFactorLengths = map (length . distinctPrimeFactors) [1..]
 
-isPrime :: Integral (a) => a -> Bool
-isPrime 1 = False
-isPrime realNumber = all ((/=) 0 . mod number) [2..isqrt number]
-	where number = abs realNumber
+distinctPrimeFactorsGenerator :: Int -> Int
+distinctPrimeFactorsGenerator d = until (\x -> all (==d) $ take d $ drop (x-1) primeFactorLengths) (+1) 1
 
-primeFactorLengths = map (length . filter isPrime . factors) [1..]
-
-distinctPrimeFactors :: Int -> Int
-distinctPrimeFactors d = until (\x -> all (==d) $ take d $ drop (x-1) primeFactorLengths) (+1) 1
-
-main = print $ distinctPrimeFactors 4
+main = print $ distinctPrimeFactorsGenerator 4
