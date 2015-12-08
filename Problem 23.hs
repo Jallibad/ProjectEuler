72f5d88297b06f02e4243ff3c0936ce4d817e8b4
Jordan <jallibad@gmail.com>
2015-12-08 10:31:12 -0500

    Improvements to Problem 23
    Improved using a map to bring the complexity from O(n^2) to O(n).
    
    (2.62 secs, 1296112568 bytes) on desktop

--- Problem 23.hs
+++ Problem 23.hs
@@ -1,9 +1,11 @@
 import Data.List
+import qualified Data.Map as Map
 import MathFunctions
 
-abundants = filter (\x -> (sum $ init $ factors x) > x) [1..]
+abundants = filter (\x -> (sum $ init $ factors x) > x) [1..28123-12]
 
-isAbundantSum n = any ((`elem` lessAbundants) . (n-)) lessAbundants
-	where lessAbundants = takeWhile (<=n) abundants
+abundantMap = Map.fromDistinctAscList $ zip abundants $ repeat 0
+
+isAbundantSum x = any (\i -> Map.member (x-i) abundantMap) $ takeWhile (<x) abundants
 
 main = print $ sum $ filter (not . isAbundantSum) [1..28123]
