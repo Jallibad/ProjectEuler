9fa0a96bf1921deb7f41ac4586a788f6c02e1b4f
Jordan <jallibad@gmail.com>
2016-02-04 23:33:39 -0500

    Problem 204
    (15.18 secs, 10921383624 bytes) on desktop

---
+++ Problem 204.hs
@@ -0,0 +1,10 @@
+import Data.List
+import MathFunctions
+
+primes' = takeWhile (<=100) primes
+
+upperBound = 10^9
+
+f x = takeWhile (<upperBound) $ map (x^) [0..]
+
+main = print $ length $ foldl1 (\f1 f2 -> concatMap (\x -> takeWhile (<=upperBound) $ map (x*) f2) f1) $ map f primes'
