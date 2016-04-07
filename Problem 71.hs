67ae5c28d1360a5a7ecb3713c804334fa70a1e64
Jordan <jallibad@gmail.com>
2016-04-07 13:06:49 -0400

    Problem 71 Fix

--- Problem 71.hs
+++ Problem 71.hs
@@ -1,4 +1,3 @@
-import Data.Function
 import Fraction
 
-main = print $ numerator $ last $ takeWhile ((<=10^6) . denominator) $ iterate (simplify . (mediant $ Fraction 3 7)) $ Fraction 0 1
+main = print $ numerator $ last $ takeWhile ((<=10^6) . denominator) $ iterate (mediant $ Fraction 3 7) $ Fraction 0 1
