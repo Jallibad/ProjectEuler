10b20dea49098b31d4e3853ca7cc94bfbe633fdb
Jordan <jallibad@gmail.com>
2016-07-10 13:54:26 -0400

    Problem 59

--- Problem 59.hs
+++ Problem 59.hs
@@ -1,16 +1,17 @@
-import Data.List
-import Data.List.Split
+import Data.List.Split (splitOn)
 import Data.Char
-import Data.Bits
+import Data.Bits (xor)
 
 keys = [[a,b,c] | a <- alphabet, b <- alphabet, c <- alphabet]
 	where alphabet = [ord 'a'.. ord 'z']
 
 display :: [Int] -> String
-display chars = map (\code -> let char = chr code in if or $ map ($ char) [isSeparator, isLetter, isNumber] then char else '_') chars
+display chars = map (\code -> let char = chr code in if any ($ char) [isSeparator, isLetter, isNumber] then char else '_') chars
 
-valid :: [Int] -> String
-valid = all (\c -> )
+valid :: [Int] -> Bool
+valid chars = a && b
+	where	a = all (isPrint . chr) chars
+		b = all (\x -> all ($ (chr x)) [(/='`'), (/='&')]) chars
 
 decrypt :: [Int] -> [Int] -> [Int]
 decrypt message key = zipWith xor message $ cycle key
@@ -18,4 +19,4 @@ decrypt message key = zipWith xor message $ cycle key
 main = do
 	file <- readFile "Problem 59 Cipher.txt"
 	let encrypted = map read $ splitOn "," file
-	print $ head $ map (map chr) $ filter valid $ map (decrypt encrypted) keys
+	print $ sum $ head $ filter valid $ map (decrypt encrypted) keys
