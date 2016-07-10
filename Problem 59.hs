3418f08efa2cc2c86160413b4df78e2a9a8ee53d
Jordan <jallibad@gmail.com>
2016-07-09 22:27:57 -0400

    First work in a while

--- Problem 59.hs
+++ Problem 59.hs
@@ -9,8 +9,13 @@ keys = [[a,b,c] | a <- alphabet, b <- alphabet, c <- alphabet]
 display :: [Int] -> String
 display chars = map (\code -> let char = chr code in if or $ map ($ char) [isSeparator, isLetter, isNumber] then char else '_') chars
 
+valid :: [Int] -> String
+valid = all (\c -> )
+
 decrypt :: [Int] -> [Int] -> [Int]
 decrypt message key = zipWith xor message $ cycle key
+
 main = do
 	file <- readFile "Problem 59 Cipher.txt"
-	print $ length $ filter (\list -> "the" `isInfixOf` list) $ map display $ zipWith decrypt (repeat $ map read $ splitOn "," file) keys
+	let encrypted = map read $ splitOn "," file
+	print $ head $ map (map chr) $ filter valid $ map (decrypt encrypted) keys
