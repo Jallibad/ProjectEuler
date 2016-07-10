2fa0877caba143055c93b36bb7cd1de7d899bd99
Jordan <jallibad@gmail.com>
2016-07-10 02:03:30 -0400

    Problem 53 Use Modules

--- Problem 53.hs
+++ Problem 53.hs
@@ -1,9 +1,3 @@
-factorials = 1 : (scanl1 (*) [1..])
-
-factorial :: Int -> Integer
-factorial n = factorials !! n
-
-combinatoric :: Integral (a) => Int -> Int -> Integer
-combinatoric n r = (factorial n) `div` ((factorial (n-r))*(factorial r))
+import MathFunctions (combinatoric)
 
 main = print $ length $ filter (>10^6) [combinatoric n r | n <- [1..100], r <- [1..n-1]]
