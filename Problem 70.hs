336b959d891f27ab336338f1e5903d2f695db703
Jordan <jallibad@gmail.com>
2018-07-30 15:20:18 -0400

    Problem 70 Mutable State
    Problem 70 sieve now uses a mutable unboxed array for the totients
    sieve, it's presumably faster but I haven't actually measured it

--- Problem 70.hs
+++ Problem 70.hs
@@ -4,7 +4,11 @@ import Data.Ratio
 import qualified Data.Map as M
 import MathFunctions
 import Data.Ord (comparing)
-import Data.Array
+import Data.Array.ST
+import Control.Monad.ST
+import Control.Monad
+
+type TotientSieve s = STUArray s Int Int
 
 {-
 --Taken from "http://programmingpraxis.com/2012/07/10/sieving-for-totients/" NOT MY WORK
@@ -13,10 +17,17 @@ totients n = M.elems $ foldl (\m i -> if m M.! i == i then foldr (M.adjust (\x -
 	(M.fromList $ zip [0..n] [0..n]) [2..n]
 -}
 
-totients :: Integer -> [(Integer, Integer)]
-totients upperBound = drop 2 $ assocs $ foldl f (listArray (0, upperBound-1) [0..]) [2..upperBound-1]
-	where f arr i
-		| arr ! i /= i = arr
-		| otherwise = accum (\j _ -> truncate $ ((fromIntegral j) / (fromIntegral i))*(fromIntegral $ i-1)) arr $ zip [i,2*i..upperBound-1] $ repeat i
+totients n = runST $ do
+	arr <- newListArray (0,n) [0..]
+	mapM_ (innerFold arr n) [2..n]
+	getAssocs arr
 
-main = print $ fst $ minimumBy (comparing $ uncurry ((/) `on` fromIntegral)) $ filter (\(x, y) -> (sort $ show x) == (sort $ show y)) $ totients $ 10^7
+innerFold arr n i = do
+	a <- readArray arr i
+	when (a == i) $
+		mapM_ (adjust arr (\x -> x*(i-1) `div` i)) [i,2*i..n]
+
+adjust :: TotientSieve s -> (Int -> Int) -> Int -> ST s ()
+adjust arr f i = readArray arr i >>= writeArray arr i . f
+
+main = print $ fst $ minimumBy (comparing $ uncurry ((/) `on` fromIntegral)) $ filter (uncurry $ on (==) (sort . show)) $ drop 2 $ totients $ 10^7
