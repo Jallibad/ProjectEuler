ad07d41a23ab744fd8d5393de1fc579dff77a374
Jordan <jallibad@gmail.com>
2018-08-07 11:23:23 -0400

    Move Totient sieve to Sieve module

--- Problem 70.hs
+++ Problem 70.hs
@@ -1,15 +1,12 @@
-import Data.Function
-import Data.List
-import Data.Ratio
-import qualified Data.Map as M
-import MathFunctions
-import Data.Ord (comparing)
-import Data.Array.ST
-import Control.Monad.ST
-import Control.Monad
-import Control.Arrow
+{-# LANGUAGE TupleSections, BangPatterns #-}
 
-type TotientSieve s = STUArray s Int Int
+import Control.Applicative ((<$>))
+import Control.Monad (foldM)
+import Control.Monad.ST (runST, ST)
+import Data.Array.ST (readArray, STUArray)
+import Data.Function (on)
+import Data.List (minimumBy, sort)
+import Sieve (totientSieve)
 
 {-
 --Taken from "http://programmingpraxis.com/2012/07/10/sieving-for-totients/" NOT MY WORK
@@ -18,28 +15,16 @@ totients n = M.elems $ foldl (\m i -> if m M.! i == i then foldr (M.adjust (\x -
 	(M.fromList $ zip [0..n] [0..n]) [2..n]
 -}
 
-totients n = do
-	arr <- newListArray (0,n) [0..]
-	mapM_ (innerFold arr n) [2..n]
-	getAssocs arr
-
-totients' n = do
-	arr <- newListArray (0,n) [0..]
-	mapM_ (innerFold arr n) [2..n]
-	return arr
-
-innerFold arr n i = do
-	a <- readArray arr i
-	when (a == i) $
-		mapM_ (adjust arr (\x -> x*(i-1) `div` i)) [i,2*i..n]
-
-adjust :: TotientSieve s -> (Int -> Int) -> Int -> ST s ()
-adjust arr f i = readArray arr i >>= writeArray arr i . f
-
 minFun :: (Fractional a, Ord a) => (Int, a) -> (Int, Int) -> (Int, a)
-minFun a b
-	| ((uncurry $ on (/=) (sort . show)) b) || ((snd a) < b') = a
-	| otherwise = (fst b, b')
-	where b' = (uncurry ((/) `on` fromIntegral)) b
+minFun !a (!i,!e)
+	| ((/=) `on` (sort . show)) i e || ((snd a) < b') = a
+	| otherwise = (i, b')
+	where b' = ((/) `on` fromIntegral) i e
 
-main = print $ fst $ minimumBy (comparing $ uncurry ((/) `on` fromIntegral)) $ filter (uncurry $ on (==) (sort . show)) $ drop 2 $ runST $ totients $ 10^7
+new n = fst $ runST $ do
+	arr <- totientSieve n :: ST s (STUArray s Int Int)
+	foldM (minReduction arr) (100,100) [2..n]
+
+minReduction arr curr i = (minFun curr . (i,)) <$> readArray arr i
+
+main = print $ new $ 10^6
--- Sieve.hs
+++ Sieve.hs
@@ -1,11 +1,28 @@
-{-# LANGUAGE TupleSections, FlexibleInstances #-}
+{-# LANGUAGE TupleSections #-}
 
 module Sieve where
 
+import Control.Monad
 import Data.Array.IArray
+import Data.Array.MArray
 
 divisorSieve :: (Integral i, Ix i, IArray a e) => (e -> i -> e) -> e -> i -> a i e
 divisorSieve f starter n = foldr addFactors emptyFactors [1..n]
 	where
 		emptyFactors = listArray (1,n) $ repeat starter
 		addFactors x arr = accum f arr $ map (,x) [2*x,3*x..n]
+
+totientSieve :: (MArray a e m, Ix e, Integral e) => e -> m (a e e)
+totientSieve n = do
+	arr <- newListArray (0,n) [0..]
+	mapM_ (innerFold arr n) [2..n]
+	return arr
+
+innerFold :: (MArray a e m, Ix e, Integral e) => a e e -> e -> e -> m ()
+innerFold arr n i = do
+	a <- readArray arr i
+	when (a == i) $
+		mapM_ (adjust arr (\x -> x*(i-1) `div` i)) [i,2*i..n]
+
+adjust :: (MArray a e m, Ix i) => a i e -> (e -> e) -> i -> m ()
+adjust arr f i = readArray arr i >>= writeArray arr i . f
