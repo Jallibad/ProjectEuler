56d211b6092577117a9199476e4cbed941db31e3
Jordan <jallibad@gmail.com>
2018-08-07 01:27:25 -0400

    Switch to Sieve module

--- Problem 21.hs
+++ Problem 21.hs
@@ -1,3 +1,12 @@
-import MathFunctions (isAmicable)
+import Data.Array.Unboxed (UArray)
+import qualified MathFunctions (amicable)
+import Sieve (divisorSieve)
+import Memoize (lookupFrom)
+
+amicable = MathFunctions.amicable `lookupFrom` (divisorSieve (+) 0 $ 10^4 :: UArray Int Int)
+
+isAmicable :: Int -> Bool
+isAmicable a = a == amicable b && a /= b
+	where b = amicable a
 
 main = print $ sum $ filter isAmicable [1..10^4-1]
