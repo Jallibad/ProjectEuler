f696e44a17b9a8afc7fb44607739a3a7d6c32cd6
Jordan <jallibad@gmail.com>
2018-08-09 11:17:58 -0400

    Problem 31 abstracted memoize stuff

--- Problem 31.hs
+++ Problem 31.hs
@@ -1,9 +1,18 @@
+{-# LANGUAGE TupleSections, ScopedTypeVariables, ViewPatterns, FlexibleContexts #-}
+
 import qualified Data.Map.Strict as Map
 import Data.Maybe
+import Control.Arrow
+import Data.STRef
+import Control.Monad
+import Control.Monad.ST
+import Control.Monad.Fix (fix)
+import Memoize
 
-coinSums n = fst $ coinSums' n 200 Map.empty
-	where	coinSums' 0 _ m = (1, m)
-		coinSums' n max m = fromMaybe (ans, Map.insert (n, max) ans m'') $ fmap (\x -> (x, m)) $ Map.lookup (n, max) m
-			where	(ans, m'') = foldl (\(prev, m') coin -> let (a, b)=coinSums' (n-coin) coin m' in (prev+a, b)) (0, m) $ takeWhile (\i -> i<=n && i<=max) [1,2,5,10,20,50,100,200]
+coinSums n = runST $ newSTRef Map.empty >>= \ref -> coinSumsM ref (n, 200)
+
+coinSumsM :: STRef s (Map.Map (Int, Int) Int) -> (Int, Int) -> ST s Int
+coinSumsM _ (0, _) = return 1
+coinSumsM ref (n, max) = fmap sum $ mapM (\coin -> memoizeHelper coinSumsM ref ((n-coin), coin)) $ takeWhile (\i -> i<=n && i<=max) [1,2,5,10,20,50,100,200]
 
 main = print $ coinSums 200
