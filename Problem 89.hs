acc4d6f1d6bb1c5d6f583007318829e1581ed36c
Jordan <jallibad@gmail.com>
2018-08-09 21:38:09 -0400

    Problem 89 Improvements

--- Problem 89.hs
+++ Problem 89.hs
@@ -1,6 +1,8 @@
+import Control.Arrow ((&&&))
+import Data.Function (on)
 import RomanNumeral
 
 charSaved :: RomanNumeral -> Int
-charSaved numeral = (length $ show numeral) - (length $ show $ simplify numeral)
+charSaved = uncurry (on (-) $ length . show) . (id &&& simplify)
 
 main = readFile "Problem 89 Roman Numerals.txt" >>= print . sum . map (charSaved . RomanNumeral) . lines
