25199bff9449314d9cf939e666b36146613ba2ce
Jordan <jallibad@gmail.com>
2018-08-09 22:04:09 -0400

    Problem 55 improvements

--- Problem 55.hs
+++ Problem 55.hs
@@ -1,14 +1,16 @@
+{-# LANGUAGE ViewPatterns #-}
+
+import Control.Arrow ((&&&))
 import ListFunctions (isPalindrome)
 
-reverseNumber :: (Show a, Integral a) => Read (a) => a -> a
-reverseNumber number = read $ reverse $ show number
+reverseNumber :: (Show a, Integral a, Read a) => a -> a
+reverseNumber = uncurry (+) . (id &&& read . reverse . show)
 
 lychrel :: Integer -> Bool
 lychrel number = lychrel' number 50
-	where	lychrel' _ 1 = True
-		lychrel' number starting
-			| isPalindrome $ show changedNumber = False
-			| otherwise = lychrel' changedNumber (starting-1)
-			where changedNumber = number + reverseNumber number
+	where
+		lychrel' _ 1 = True
+		lychrel' (reverseNumber -> number) starting =
+			(not $ isPalindrome $ show number) && (lychrel' number (starting-1))
 
 main = print $ length $ filter lychrel [1..10^4]
