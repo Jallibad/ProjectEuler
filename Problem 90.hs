d233cb0eb1a05a2a0a9cc894a4df7b32b5806331
Jordan <jallibad@gmail.com>
2018-08-09 21:47:26 -0400

    Problem 90 improvements

--- Problem 90.hs
+++ Problem 90.hs
@@ -6,7 +6,8 @@ squares = ["01","04","06","16","18","25","36","46"]
 options = pick 6 $ ['0'..'9']
 
 validCombination (as,bs) = null $ squares \\ [sort [a,b] | a <- map sixToNine as, b <- map sixToNine bs]
-	where	sixToNine '9' = '6'
+	where
+		sixToNine '9' = '6'
 		sixToNine x = x
 
-main = print $ length $ filter validCombination [(a,b) | a <- options, b <- takeWhile (<=a) options]
+main = print $ length [(a,b) | a <- options, b <- takeWhile (<=a) options, validCombination (a,b)]
