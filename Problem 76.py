4e0f573575c5e8153682d3f46d09518300cf4707
Jordan <jallibad@gmail.com>
2018-08-09 21:53:17 -0400

    Problem 76 improved Python memoization

--- Problem 76.py
+++ Problem 76.py
@@ -1,12 +1,12 @@
 memoize = {}
-def countSums(number):
-    return countSumsR(number, number-1)
-def countSumsR(number, currMax):
+def countSums(number, currMax=None):
     if number==0:
         return 1
-    elif (number, currMax) not in memoize:
+    if currMax is None:
+        currMax = number-1
+    if (number, currMax) not in memoize:
         memoize[(number, currMax)] = 0
         for x in range(1, min(number, currMax)+1):
-            memoize[(number, currMax)] += countSumsR(number-x, min(x,number-x))
+            memoize[(number, currMax)] += countSums(number-x, min(x,number-x))
     return memoize[(number, currMax)]
 print(countSums(100))
