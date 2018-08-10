bd918fbca85c0ce1ec430e04920ecc2bd873c4df
Jordan <jallibad@gmail.com>
2018-08-09 22:36:35 -0400

    Problem 69 switched to Sieve module

--- Problem 69.hs
+++ Problem 69.hs
@@ -1,12 +1,9 @@
-import Data.List
-import Data.Ratio
+import Data.Array.IO (getAssocs, IOUArray)
+import Data.Function (on)
+import Data.List (maximumBy)
 import Data.Ord (comparing)
-import qualified Data.Map as M
+import Sieve (totientSieve)
 
---Taken from "http://programmingpraxis.com/2012/07/10/sieving-for-totients/" NOT MY WORK
-totients :: Integral a => a -> [a]
-totients n = M.elems $ foldl (\m i -> if m M.! i == i
-    then foldr (M.adjust (\x -> div (x*(i-1)) i)) m [i,2*i..n] else m)
-    (M.fromList $ zip [0..n] [0..n]) [2..n]
-
-main = print $ fst $ maximumBy (comparing snd) $ tail $ zipWith (\x y -> (x, x/(fromIntegral y))) [0..10^6] $ totients (10^6)
+main = (totientSieve $ 10^6 :: IO (IOUArray Int Int)) >>= getAssocs >>=
+	print . fst . maximumBy cmp . tail
+	where cmp = comparing $ uncurry $ on (/) fromIntegral
