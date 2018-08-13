6cc0ecc714be73a5f88be384e88b9aeb48db702f
Jordan <jallibad@gmail.com>
2018-08-13 10:11:05 -0400

    Working on memoizing problem 78

--- Problem 78.hs
+++ Problem 78.hs
@@ -1,13 +1,15 @@
 import Data.Function
+import Data.List (genericIndex)
 
 --NOT MINE - taken from "http://math.stackexchange.com/questions/736008/understanding-the-partition-function"
-p :: Int -> Int -> Int
-p m n
+p :: ((Integer, Integer) -> Integer) -> (Integer, Integer) -> Integer
+p recurse (m,n)
 	| n < 0  = 0
 	| n == 0 = 1
-	| otherwise = sum [p i (n - i) | i <- [m..n]]
+	| otherwise = sum [recurse (i, (n - i)) | i <- [m..n]]
 
-memoize :: (Int -> a) -> (Int -> a)
-memoize f = (map f [0 ..] !!)
+partitions :: [[Integer]]
+partitions = map (\n -> map (\m -> p partition (m,n)) [1..n]) [1..]
 
-partitionMemo = fix (memoize . p)
+partition :: (Integer, Integer) -> Integer
+partition (m, n) = (partitions `genericIndex` n) `genericIndex` m
