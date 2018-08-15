9c76bdcf5a2f88ee59e173c43a92638c65f93149
Jordan <jallibad@gmail.com>
2018-08-15 18:26:40 -0400

    Shortened Problem 123

--- Problem 123.hs
+++ Problem 123.hs
@@ -1,3 +1,3 @@
 import MathFunctions (primes)
 
-main = print $ fst $ head $ dropWhile ((<=10^10) . snd) $ map (\(n, p) -> (n, ((p-1)^n+(p+1)^n) `mod` (p^2))) $ zip [1..] primes
+main = print $ fst $ head $ dropWhile (\(n, p) -> ((p-1)^n+(p+1)^n) `mod` (p^2) <=10^10) $ zip [1..] primes
