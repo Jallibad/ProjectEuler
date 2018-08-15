9aec5c833e586894e30866cb946b9b5a530387b0
Jordan <jallibad@gmail.com>
2018-08-15 14:27:44 -0400

    Solved Problem 188
    Other minor improvements as well

---
+++ Problem 188.hs
@@ -0,0 +1,11 @@
+﻿tetrate :: Integral a => a -> a -> a -> a
+tetrate a 1 _ = a
+tetrate a k b = modular_pow a (tetrate a (k-1) b) b
+
+mult x y b = (x*y) `mod` b
+
+modular_pow _ 0 _ = 1
+modular_pow base exponent modulus = if odd exponent then mult result base modulus else result
+	where result = modular_pow (mult base base modulus) (exponent `div` 2) modulus
+
+main = print $ tetrate 1777 1855 $ 10^8
--- Problem 25.hs
+++ Problem 25.hs
@@ -1,3 +1,5 @@
 import MathFunctions (fibs)
 
-main = print $ fst $ head $ until ((==1000) . length . show . snd . head) tail $ zip [0..] fibs
+fib = (fibs !!)
+
+main = print $ until ((==1000) . length . show . fib) succ 0
