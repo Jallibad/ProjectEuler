c6accaeac7f8698b6291ac3e17e7dd66438a3d04
Jordan <jallibad@gmail.com>
2018-11-06 13:27:46 -0500

    Rewrote Poblem 102 to use Parsec

--- Problem 102.hs
+++ Problem 102.hs
@@ -1,28 +1,42 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes #-}
+
+import Control.Monad (void)
+import Text.Parsec (Stream, ParsecT, (<|>), char, digit, endOfLine, many, many1, parse)
+
 data Point = Point {x :: Int, y :: Int} deriving (Show)
 data Triangle = Triangle {a :: Point, b :: Point, c :: Point} deriving (Show)
 
+type Parser a = forall s u m. Stream s m Char => ParsecT s u m a
+
+unsignedInteger :: (Integral a, Read a) => Parser a
+unsignedInteger = read <$> many1 digit
+
+integer :: (Integral a, Read a) => Parser a
+integer = (char '-' >> negate <$> unsignedInteger) <|> unsignedInteger
+
+comma :: Parser ()
+comma = void $ char ','
+
+point :: Parser Point
+point = Point <$> integer <* comma <*> integer
+
+triangle :: Parser Triangle
+triangle = Triangle <$> point <* comma <*> point  <* comma <*> point
+
+triangles :: Parser [Triangle]
+triangles = many $ triangle <* endOfLine
+
 containsOrigin :: Triangle -> Bool
-containsOrigin (Triangle (Point x1 y1) (Point x2 y2) (Point x3 y3)) = (0 <= t1) && (t1 <= 1) && (0 <= t2) && (t2 <= 1) && (s <= 1)
+containsOrigin (Triangle (Point x1 y1) (Point x2 y2) (Point x3 y3)) = between 0 1 t1 && between 0 1 t2 && s <= 1
 	where
 		denominator = (y2-y3)*(x1-x3)+(x3-x2)*(y1-y3)
 		t1 = (fromIntegral $ (y2-y3)*(-x3)+(x3-x2)*(-y3)) / (fromIntegral denominator)
 		t2 = (fromIntegral $ (y3-y1)*(-x3)+(x1-x3)*(-y3)) / (fromIntegral denominator)
+		between l u n = l <= n && n <= u
 		s = t1+t2
-
-readTriangle :: [Point] -> Triangle
-readTriangle (a:b:c:[]) = Triangle a b c
-readTriangle _ = error "wrong number of points"
-
-readPoints :: [Int] -> [Point]
-readPoints [] = []
-readPoints ints = (Point x y) : readPoints xs
-	where (x:y:[], xs) = splitAt 2 ints
-
-readInts :: String -> [Int]
-readInts [] = []
-readInts string = (read f) : (readInts $ drop 1 s)
-	where (f, s) = break (==',') string
 
 main = do
 	file <- readFile "Problem 102 Triangles.txt"
-	print $ length $ filter (containsOrigin . readTriangle . readPoints . readInts) $ lines file
+	let numContainingOrigin = length . filter containsOrigin
+	print $ numContainingOrigin <$> parse triangles "" file
